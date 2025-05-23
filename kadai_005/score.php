3c6821524f96b08c106dfcd4aad4259c60ca5bc6
wakobe <be.wako2025@gmail.com>
2025-05-23 13:44:03 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>課題5</title>
+</head>
+<body>
+    <p>
+        <?php
+        $score1 = 80;
+        $score2 = 60;
+        $score3 = 55;
+        $score4 = 40;
+        $score5 = 100;
+        $score6 = 25;
+        $score7 = 80;
+        $score8 = 95;
+        $score9 = 30;
+        $score10 = 60;
+
+        $total = $score1+$score2+$score3+$score4+
+                    $score5+$score6+$score7+$score8+
+                    $score9+$score10;
+
+        $ave = $total / 10;
+
+        echo $ave
+        ?>
+    </P>
+</body>
+</html>

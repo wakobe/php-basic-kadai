0dc49daf62bb2f7900206da5f458536da25797e1
wakobe <be.wako2025@gmail.com>
2025-05-23 13:45:34 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>課題7</title>
+</head>
+<body>
+    <p>
+        <?php
+       $array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+       print_r($array);
+
+       ?>
+    </P>
+</body>
+</html>

dac94cf6ff322a674725d241386fca60e7266312
wakobe <be.wako2025@gmail.com>
2025-05-29 13:47:28 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,11 +9,11 @@
 <body>
     <p>
        <?php
-function sort_2way(array $nums, string $order = "asc"): void {
-    if ($order === "asc") {
-        sort($nums); // 昇順
+function sort_2way(array $nums, bool $order): void {
+    if ($order) {
+        sort($nums);   // 昇順
     } else {
-        rsort($nums); // 降順
+        rsort($nums);  // 降順
     }
 
     foreach ($nums as $num) {
@@ -25,11 +25,12 @@ function sort_2way(array $nums, string $order = "asc"): void {
 $nums = [15, 4, 18, 23, 10];
 
 echo "昇順にソートします。<br>";
-sort_2way($nums, "asc");
+sort_2way($nums, true);   // true = 昇順
 
 echo "<br>降順にソートします。<br>";
-sort_2way($nums, "desc");
+sort_2way($nums, false);  // false = 降順
 ?>
+
 
     </p>
 </body>
